e0214170c5ae33bd6c63c2be7e64ad3bf95f3491
Anusha <cdan.dharmasena@gmail.com>
2023-11-23 20:51:28 +1100

    add jest extended support and ts-jest preset to test typescript files with types

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  preset: "ts-jest",
+  setupFilesAfterEnv: ["jest-extended/all"],
   testEnvironment: "node",
   verbose: true,
 };
